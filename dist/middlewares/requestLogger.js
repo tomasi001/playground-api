f32d83c6f641d211386f3cdd5630d671c5bd7ae4
Thomas-Sov <137511702+Thomas-Sov@users.noreply.github.com>
2024-09-27 16:02:37 +0200

    Stop tracking dist folder

--- dist/middlewares/requestLogger.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.requestLogger = void 0;
-const requestLogger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-};
-exports.requestLogger = requestLogger;
--- dist/routes/openaiRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const openaiController_1 = require("../controllers/openaiController");
-const router = (0, express_1.Router)();
-router.post('/openai', openaiController_1.getOpenAIResponse);
-exports.default = router;
